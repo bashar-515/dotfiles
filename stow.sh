9756cc7e4166a28e69a26fd143b7d7bd30513f7a
Bashar Eid <bashar.eid@viam.com>
2025-02-12 23:46:55 -0500

    fix syntax error

--- stow.sh
+++ stow.sh
@@ -5,13 +5,13 @@ DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 DOTFILES=$(ls -d */ | sed 's#/##')
 
 stow_dotfiles() {
-    for dir in $DOTFILES
-        stow -d "$DOTFILES_DIR" -t "$HOME" "$dir"
+    for dotfile in $DOTFILES; do
+        stow -d "$DOTFILES_DIR" -t "$HOME" "$dotfile"
     done
 }
 
 if ! command -v stow &> /dev/null; then
-    echo "GNU Stow is not installed"
+    echo "GNU Stow is not installed."
     exit 1
 fi
 
