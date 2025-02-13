f3dfb785cf5e08e3408eb8ca02b5bf497d34cabf
Bashar Eid <bashar.eid@viam.com>
2025-02-13 01:44:21 -0500

    Revert "move lazy file up a level"
    This reverts commit ab07ca52103c588e7f1d3cc75b559f9d806f1bbf.

--- nvim/.config/nvim/lua/config/lazy.lua
+++ nvim/.config/nvim/lua/config/lazy.lua
@@ -19,6 +19,8 @@ vim.opt.rtp:prepend(lazypath)
 -- loading lazy.nvim so that mappings are correct.
 -- This is also a good place to setup other settings (vim.opt)
 require("core")
+-- vim.g.mapleader = " "
+-- vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
@@ -28,6 +30,7 @@ require("lazy").setup({
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
+  install = { colorscheme = { "habamax" } },
   -- automatically check for plugin updates
   checker = { enabled = true },
 })
