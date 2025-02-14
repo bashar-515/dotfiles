d008d43479a2722e61e8856adc7556977afebc97
Bashar Eid <bashar.eid@viam.com>
2025-02-14 16:30:05 -0500

    create profiles

--- Darwin/zsh/.zshrc
+++ Darwin/zsh/.zshrc
@@ -1,4 +1,5 @@
 eval "$(starship init zsh)"
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
---
+++ setup.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+DOTFILES_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+COMMON_DIR="$DOTFILES_DIR/common"
+OS_DIR="$DOTFILES_DIR/$(uname)"
+
+stow_dotfiles() {
+    # absolute path to either Linux, Darwin, or common directories
+    local program_dir=$1
+
+    # names of directories in `program_dir`
+    local dotfiles=$(ls -d "$program_dir"/*/ | xargs -n1 basename)
+
+    for dotfile in $dotfiles; do
+        for entry in "$program_dir/$dotfile"/{*,.[!.]*}; do
+            local path=$HOME/$(basename "$entry")
+
+            if [[ -e "$path" ]]; then
+                mv "$path" "${path}.$(date -u +%Y%m%d%H%M%S).bak"
+            fi
+        done
+    done
+
+
+    for dotfile in $dotfiles; do
+        # e.g., stow -d ~/.files/common -t ~/. nvim
+        stow -d "$program_dir" -t "$HOME" "$dotfile"
+    done
+}
+
+if ! command -v stow &> /dev/null; then
+    echo "GNU Stow is not installed."
+    exit 1
+fi
+
+if [[ ! -d "$COMMON_DIR" ]]; then
+    echo "$COMMON_DIR does not exist"
+    exit 1
+fi
+
+if [[ ! -d "$OS_DIR" ]]; then
+    echo "$OS_DIR does not exist"
+    exit 1
+fi
+
+stow_dotfiles "$COMMON_DIR"
+stow_dotfiles "$OS_DIR"
