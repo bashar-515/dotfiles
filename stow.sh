6ab01b9109d895a3dbeb5be6448cd4c535c398c0
Bashar Eid <bashar.eid@viam.com>
2025-02-14 02:00:38 -0500

    stop using variable names

--- stow.sh
+++ stow.sh
@@ -12,22 +12,10 @@ OS_DIR="$DOTFILES_DIR/$OS"
 DOTFILES_COMMON=$(ls -d "$COMMON_DIR"/*/ | xargs -n1 basename)
 DOTFILES_OS=$(ls -d "$OS_DIR"/*/ | xargs -n1 basename)
 
-
 stow_dotfiles() {
-    local dir=$1
-    local dotfiles=$2
-
     for dotfile in $dotfiles; do
-        # for file in "$dir/$dotfile"/*; do
-        #     [ -e "$file" ] || continue  # Skip if no files exist
-        #
-        #     filename=$(basename "$file")
-        #
-        #     echo "Processing file: $filename"
-        # done
-
         # e.g., stow -d ~/.files/common -t ~/. nvim
-        stow -d "$dir" -t "$HOME" "$dotfile"
+        stow -d "$1" -t "$HOME" "$2"
     done
 }
 
@@ -37,9 +25,13 @@ if ! command -v stow &> /dev/null; then
 fi
 
 if [[ -d "$COMMON_DIR" ]]; then
+    echo "$COMMON_DIR does not exist"
+    exit 1
     stow_dotfiles "$COMMON_DIR" "$DOTFILES_COMMON"
 fi
 
 if [[ -d "$OS_DIR" ]]; then
+    echo "$OS_DIR does not exist"
+    exit 1
     stow_dotfiles "$OS_DIR" "$DOTFILES_OS"
 fi
