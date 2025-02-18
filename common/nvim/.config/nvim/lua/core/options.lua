9742a87c6ec966a6ba51046f35385523294b7dfe
Bashar Eid <bashar.eid@viam.com>
2025-02-17 22:07:59 -0500

    configure starship and add some nvim plugins

--- common/nvim/.config/nvim/lua/core/options.lua
+++ common/nvim/.config/nvim/lua/core/options.lua
@@ -1,3 +1,27 @@
 local opt = vim.opt
 
 opt.relativenumber = true
+opt.number = true
+
+opt.tabstop = 4 
+opt.shiftwidth = 4 
+opt.expandtab = true 
+opt.autoindent = true 
+
+opt.ignorecase = true 
+opt.smartcase = true 
+
+opt.cursorline = true
+
+opt.signcolumn = "yes"
+
+opt.backspace = "indent,eol,start"
+
+opt.clipboard:append("unnamedplus")
+
+opt.splitright = true
+opt.splitbelow = true
+
+opt.scrolloff = 8
+opt.wrap = false
+opt.undofile = true
