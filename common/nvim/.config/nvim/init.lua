62fbe99676d591ee55d36529c9ce58d294ff42d5
Bashar Eid <beesho.eid@gmail.com>
2025-02-23 23:02:30 -0500

    only source .vimrc file

--- common/nvim/.config/nvim/init.lua
+++ common/nvim/.config/nvim/init.lua
@@ -1,12 +1,9 @@
 require("config")
 
-vim.cmd([[
-  set runtimepath^=~/.vim runtimepath+=~/.vim/after
-  let &packpath = &runtimepath
-]])
-
 if vim.fn.filereadable(vim.fn.expand("~/.vimrc")) == 1 then
   vim.cmd([[
     source ~/.vimrc
   ]])
+else
+  require("core")
 end
