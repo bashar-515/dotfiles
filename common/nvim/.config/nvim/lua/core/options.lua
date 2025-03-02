b6381e7e0c43f501236422e3a482d0754948101c
Bashar Eid <beesho.eid@gmail.com>
2025-03-02 12:32:13 -0500

    delete trailing spaces

--- common/nvim/.config/nvim/lua/core/options.lua
+++ common/nvim/.config/nvim/lua/core/options.lua
@@ -3,13 +3,13 @@ local opt = vim.opt
 opt.relativenumber = true
 opt.number = true
 
-opt.tabstop = 4 
-opt.shiftwidth = 4 
-opt.expandtab = true 
-opt.autoindent = true 
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
 
-opt.ignorecase = true 
-opt.smartcase = true 
+opt.ignorecase = true
+opt.smartcase = true
 
 opt.cursorline = true
 
