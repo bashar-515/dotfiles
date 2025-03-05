bd7422a0804e025538afc47c3bed28a210872e2a
Bashar Eid <beesho.eid@gmail.com>
2025-03-05 00:34:26 -0500

    use rose-pine theme

--- common/nvim/.config/nvim/lua/plugins/theme.lua
+++ common/nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,9 +1,17 @@
 -- install and set theme
 return {
-	"sainnhe/sonokai",
-	priority = 1000,
+	"rose-pine/neovim",
+	name = "rose-pine",
 
 	config = function()
-		vim.cmd("colorscheme sonokai")
+		require("rose-pine").setup({
+			variant = "moon",
+
+			styles = {
+				transparency = true,
+			},
+		})
+
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
