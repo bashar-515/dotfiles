ee0a54fd4c84789031310add2329702c8c32de55
Bashar Eid <beesho.eid@gmail.com>
2025-03-04 21:39:23 -0500

    use bufflerline plugin

---
+++ common/nvim/.config/nvim/lua/plugins/bufferline.lua
@@ -0,0 +1,11 @@
+-- pretty and functional tabs
+return {
+	"akinsho/bufferline.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	version = "*",
+	opts = {
+		options = {
+			mode = "tabs",
+		},
+	},
+}
