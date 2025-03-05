96b739d7736c111179f9a985e036d0403c135074
Bashar Eid <beesho.eid@gmail.com>
2025-03-04 22:25:00 -0500

    format treesitter

--- common/nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ common/nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,33 +1,33 @@
 -- syntax highlighting
 return {
-  "nvim-treesitter/nvim-treesitter",
+	"nvim-treesitter/nvim-treesitter",
 
-  -- lazy load
-  event = { "BufReadPre", "BufNewFile" },
+	-- lazy load
+	event = { "BufReadPre", "BufNewFile" },
 
-  config = function()
-    local configs = require("nvim-treesitter.configs")
+	config = function()
+		local configs = require("nvim-treesitter.configs")
 
-    configs.setup({
-      -- the listed parsers MUST always be installed
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "markdown",
-        "markdown_inline"
-      },
+		configs.setup({
+			-- the listed parsers MUST always be installed
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"query",
+				"markdown",
+				"markdown_inline",
+			},
 
-      -- install parsers synchronously/sequentially (only applied to `ensure_installed`)
-      sync_install = false,
+			-- install parsers synchronously/sequentially (only applied to `ensure_installed`)
+			sync_install = false,
 
-      -- do not install automatically detected missing parser
-      auto_install = false,
+			-- do not install automatically detected missing parser
+			auto_install = false,
 
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end,
+			highlight = { enable = true },
+			indent = { enable = true },
+		})
+	end,
 }
