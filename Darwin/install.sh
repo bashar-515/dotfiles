35a065f3134642a98984bd5ec16d0da93e77919f
Bashar Eid <beesho.eid@gmail.com>
2025-03-12 00:42:16 -0400

    fix xcode bug and no longer install git completions for zsh

--- Darwin/install.sh
+++ Darwin/install.sh
@@ -2,15 +2,8 @@
 
 set -e
 
-HOME_DIR=$(realpath "$1")
-
-if [ ! -e "$HOME_DIR" ]; then
-  echo "$HOME_DIR does not exist." >&2
-  exit 1
-fi
-
-if xcode-select -p &> /dev/null; then
-    echo "Xcode Command Line Tools are installed."
+if ! xcode-select -p &> /dev/null; then
+    echo "Xcode Command Line Tools are not installed."
 fi
 
 if ! command -v brew &> /dev/null; then
@@ -21,19 +14,6 @@ fi
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd SCRIPT_DIR
+cd $SCRIPT_DIR
 
 brew bundle
-
-# set up Git completions
-GIT_VERSION=$(git --version | awk '{print $3}')
-if [[ -z "$GIT_VERSION" ]]; then
-    echo "Unable to determine git version."
-    exit 1
-fi
-TAG="v$GIT_VERSION"
-GIT_COMPLETION_FILE="git-completion.zsh"
-FILE_PATH="contrib/completion/$GIT_COMPLETION_FILE"
-URL="https://raw.githubusercontent.com/git/git/refs/tags/${TAG}/${FILE_PATH}"
-DEST_FILE="$HOME_DIR/.$GIT_COMPLETION_FILE"
-curl -o "${DEST_FILE}" "${URL}"
