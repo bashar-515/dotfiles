218df6decc0531247641e429664a5cae02190768
Bashar Eid <beesho@MacBookPro.lan>
2025-03-11 23:11:17 -0400

    build Neovim from source

--- Linux/install.sh
+++ Linux/install.sh
@@ -9,37 +9,48 @@ fi
 
 apt-get update
 
+# install my preferred tools and required packages
 apt-get install -y \
     build-essential \
     git \
     curl \
     stow \
     tmux \
-    neovim \
     ripgrep \
     wget \
     fzf \
     bat \
     git-delta \
     eza \
-    zoxide
+    zoxide \
+    unzip
 
-if ! command -v curl >/dev/null 2>&1; then
-    echo "Curl is not installed."
-    exit 1
-fi
+# install prerequisites for building Neovim from source
+apt-get install -y \
+  ninja-build \
+  gettext \
+  cmake \
+  file
 
+# set up Git completions
 GIT_VERSION=$(git --version | awk '{print $3}')
 if [[ -z "$GIT_VERSION" ]]; then
     echo "Unable to determine git version."
     exit 1
 fi
-
 TAG="v$GIT_VERSION"
 GIT_COMPLETION_FILE="git-completion.bash"
 FILE_PATH="contrib/completion/$GIT_COMPLETION_FILE"
 URL="https://raw.githubusercontent.com/git/git/refs/tags/${TAG}/${FILE_PATH}"
 DEST_FILE=".$GIT_COMPLETION_FILE"
-
-curl -sS https://starship.rs/install.sh | sh -s -- -y
 curl -o "${DEST_FILE}" "${URL}" 
+
+# install Starship
+curl -sS https://starship.rs/install.sh | sh -s -- -y
+
+# build Neovim from source
+git clone https://github.com/neovim/neovim
+cd neovim
+git checkout stable
+make CMAKE_BUILD_TYPE=Release
+cd build && cpack -G DEB && dpkg -i nvim-linux-arm64.deb
